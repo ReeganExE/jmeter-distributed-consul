979c394cb75ac8a106afba6f402f726ee4e47242
ReeganExE <dongian.rapclubkhtn@gmail.com>
2019-03-17 16:00:41 +0700

    Rename

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t jmeter-slave -f src/r.Dockerfile src
+docker build -t jmeter-slave -f src/slave.Dockerfile src
 docker build -t jmeter-ctl -f src/ctl.Dockerfile src
